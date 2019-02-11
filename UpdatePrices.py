04c35b8b53845475922627500634dde8c80e069d
Kevin Stechler <kevin.stechler@willowtreeapps.com>
2019-02-11 12:02:48 -0500

    Reformatted code. Removed sleep statement since the API issues were on the google side.

--- UpdatePrices.py
+++ UpdatePrices.py
@@ -17,54 +17,60 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
+
 def main():
     sheet = client.open(spreadsheetName).sheet1
-    
-    cardNames=sheet.col_values(cardNameColumn)  
-    foil=sheet.col_values(foilFlagColumn)
+
+    cardNames = sheet.col_values(cardNameColumn)
+    foil = sheet.col_values(foilFlagColumn)
 
     header = True
 
-    for i in range (len(cardNames)):
-        if(header):
+    for i in range(len(cardNames)):
+        if (header):
             header = False
         else:
             currCard = make_card(cardNames[i], checkFoil(foil[i]))
             price = "$" + getPriceFor(currCard.name, currCard.foil)
             print("cost of " + currCard.name + " is " + price)
-            sheet.update_cell(i+1, priceColumn, price)
-        time.sleep(.5)
+            sheet.update_cell(i + 1, priceColumn, price)
+
 
 class Card(object):
     name = ""
     foilFlag = bool
     foil = bool
-    # The class "constructor" - It's actually an initializer 
+
+    # The class "constructor" - It's actually an initializer
     def __init__(self, name, foilFlag):
         self.name = name
         self.foil = foilFlag
+
 
 def make_card(name, foil):
     card = Card(name, foil)
     return card
 
-def getPriceFor(cardName, foilFlag):  
+
+def getPriceFor(cardName, foilFlag):
     resp = requests.get('https://api.scryfall.com/cards/named?fuzzy=' + cardName)
     json_resp = resp.json()
-    dump=json.dumps(json_resp)
-    data=json.loads(dump)
-    if((foilFlag) and (data['prices']['usd_foil'] != None)):
+    dump = json.dumps(json_resp)
+    data = json.loads(dump)
+    if ((foilFlag) and (data['prices']['usd_foil'] != None)):
         return data['prices']['usd_foil']
-    elif((not foilFlag) and (data['prices']['usd'] != None)):
+    elif ((not foilFlag) and (data['prices']['usd'] != None)):
         return data['prices']['usd']
     else:
         return "No Price Data"
 
+
 def checkFoil(foilFlag):
-    if(foilFlag == "Yes"):
+    if (foilFlag == "Yes"):
         return True
     else:
         return False
+
 
 if __name__ == '__main__':
     main()
